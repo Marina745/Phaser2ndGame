43ad3c71ab14bbef835ae1872994a62748e2b3dc
Marina745 <158029971+Marina745@users.noreply.github.com>
2024-03-13 11:21:08 +0200

    нові функції

--- game.js
+++ game.js
@@ -27,8 +27,8 @@ var scoreText;  //текстова зміна очків
 var bombs;  //бомби
 var game;
 var worldWidth = 9600;  //ширина світу
-var lives=3;    //кількість життів
-var livesText;  //текстова зміна життів
+var life = 5;    //кількість життів
+var lifesText;  //текстова зміна життів
 var game = new Phaser.Game(config);
 var resetButton;    //кнопка перезапуску
 var hearts; //життя
@@ -51,12 +51,12 @@ function preload() {
 
 function create() {
 
-        //фон плитки
+    //фон плитки
     //this.add.image(400, 300, 'sky');
     this.add.tileSprite(0, 0, worldWidth, 1080, "sky")
-    .setOrigin(0, 0)
-    .setScale(1)
-    .setDepth(0);
+        .setOrigin(0, 0)
+        .setScale(1)
+        .setDepth(0);
 
 
     // додаємо платформи на весь екран
@@ -85,14 +85,14 @@ function create() {
     //Додаємо камінці на всю ширину екрану
     for (var x = 500; x < worldWidth; x = x + Phaser.Math.FloatBetween(300, 1600)) {
         //console.log(' x-' + x)
-        stone.create(x, 1080 - 80, 'stone').setOrigin(0, 1).setScale(Phaser.Math.FloatBetween(0.5, 1)).refreshBody().setDepth(Phaser.Math.FloatBetween(1,10));
+        stone.create(x, 1080 - 80, 'stone').setOrigin(0, 1).setScale(Phaser.Math.FloatBetween(0.5, 1)).refreshBody().setDepth(Phaser.Math.FloatBetween(1, 10));
     }
     //додаємо траву
     grass = this.physics.add.staticGroup();
     //Додаємо траву на всю ширину екрану
     for (var x = 500; x < worldWidth; x = x + Phaser.Math.FloatBetween(300, 1600)) {
-       // console.log(' x-' + x)
-        grass.create(x, 1080 - 80, 'grass').setOrigin(0, 1).setScale(Phaser.Math.FloatBetween(0.2, 0.9)).refreshBody().setDepth(Phaser.Math.FloatBetween(1,10)) ;
+        // console.log(' x-' + x)
+        grass.create(x, 1080 - 80, 'grass').setOrigin(0, 1).setScale(Phaser.Math.FloatBetween(0.2, 0.9)).refreshBody().setDepth(Phaser.Math.FloatBetween(1, 10));
     }
 
     //додаємо дерево
@@ -101,26 +101,25 @@ function create() {
     for (var x = 500; x < worldWidth; x = x + Phaser.Math.FloatBetween(300, 1600)) {
         //console.log(' x-' + x)
         tree
-        .create(x, 1080 - 80, 'tree')
-        .setOrigin(0, 1)
-        .setScale(Phaser.Math.FloatBetween(0.5, 1))
-        .refreshBody()
-        .setDepth(Phaser.Math.FloatBetween(1,10))
+            .create(x, 1080 - 80, 'tree')
+            .setOrigin(0, 1)
+            .setScale(Phaser.Math.FloatBetween(0.5, 1))
+            .refreshBody()
+            .setDepth(Phaser.Math.FloatBetween(1, 10))
     }
 
     //гравець
     player = this.physics.add.sprite(100, 450, 'dude');
     player
-    .setBounce(0.2)
-    .setCollideWorldBounds(false)
-    .setDepth(5)
+        .setBounce(0.2)
+        .setCollideWorldBounds(false)
+        .setDepth(5)
 
     player.setCollideWorldBounds(true);
 
-    
+//
     this.cameras.main.setBounds(0, 0, worldWidth, window.innerHeight);
     this.physics.world.setBounds(0, 0, worldWidth, window.innerHeight);
-
     this.cameras.main.startFollow(player);
 
     // анімація в ліво
@@ -152,20 +151,20 @@ function create() {
         repeat: 30,
         setXY: { x: 12, y: 0, stepX: 110 }
     });
-    
+
     //життя
     hearts = this.physics.add.group({
         key: 'hearts',
         repeat: 10,
         setXY: { x: 12, y: 0, stepX: 550 }
-    
-        
+
+
     });
 
     //фізика для життів
-        this.physics.add.collider(hearts, platforms);
+    this.physics.add.collider(hearts, platforms);
 
-        // this.physics.add.overlap(player, hearts, collectHearts, null, this);
+    // this.physics.add.overlap(player, hearts, collectHearts, null, this);
 
     stars.children.iterate(function (child) {
 
@@ -174,27 +173,38 @@ function create() {
     });
 
     // фізика для зірок
-        this.physics.add.collider(stars, platforms);
+    this.physics.add.collider(stars, platforms);
 
-        this.physics.add.overlap(player, stars, collectStar, null, this);
+    this.physics.add.overlap(player, stars, collectStar, null, this);
 
 
-// фізика для гравця
+    // фізика для гравця
     this.physics.add.collider(player, platforms);
-  
+
     // текст рахунку
-    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+    scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '42px', fill: '#000' });
     //текст життів
-    livesText = this.add.text(1000, 0, 'Lives: = 3', { fontSize: '30px', fill: '#000' })
-    .setOrigin(0,0)
-   .setScrollFactor(0)
-   
-//фізика бомб
+    lifeText = this.add.text(1500, 100, showLife(), { fontSize: '40px', fill: '#000' })
+        .setOrigin(0, 0)
+        .setScrollFactor(0)
+        //Кнопка перезапуск гри
+        var resetButton = this.add.text(400, 450, 'reset', { fontSize: '40px', fill: '#ccc' })
+        .setInteractive()
+        .setScrollFactor(0);
+
+        resetButton.on('pointerdown', function (){
+            console.log('restart')
+            refreshBody()
+        }
+        )
+
+
+    //фізика бомб
     bombs = this.physics.add.group();
     this.physics.add.collider(bombs, platforms);
     this.physics.add.collider(player, bombs, hitBomb, null, this);
-     
-   
+
+
     this.physics.world.setBounds(0, 0, Number.MAX_SAFE_INTEGER, 1000);
     this.cameras.main.startFollow(player);
 }
@@ -202,14 +212,14 @@ function create() {
 
 
 function update() {
-  // якщо натиснута стрілка вліво 
-   if (cursors.left.isDown) { 
+    // якщо натиснута стрілка вліво 
+    if (cursors.left.isDown) {
         player.setVelocityX(-160);
 
         player.anims.play('left', true);
     }
-  // якщо натиснута стрілка вправо 
-   else if (cursors.right.isDown) {
+    // якщо натиснута стрілка вправо 
+    else if (cursors.right.isDown) {
         player.setVelocityX(160);
 
         player.anims.play('right', true);
@@ -225,19 +235,34 @@ function update() {
     }
 }
 
+//формування смуги життя
+function showLife() {
+    var lifeLine = ''
+
+    for (var i = 0; i < life; i++) {
+        lifeLine += '💕'
+        //console.log(life)
+    }
+    return lifeLine
+}
+
+//функція торкання бомб з  гравцем
 function hitBomb(player, bomb) {
-   lives += -1;
-   livesText.setText('Lives: '+ lives);
-        if ('Lives: = 0') {
-    
-        this.physics.pause(); // зупинити гру
-        player.setTint(0xff0000); // замалювати гравця червоним кольором 
-        gameOver = true;
-}  
+    //this.physics.pause();
+    bomb.disableBody(true, true);
+
+    player.setTint(0xff0000); // замалювати гравця червоним кольором 
+    life -= 1;
+    lifeText.setText(showLife());
+
+    console.log('boom')
+    player.anims.play('turn');
+
+    if (life == 0) gameOver = true;
 }
 
 
-   
+
 function collectStar(player, star) {
     star.disableBody(true, true);
 
@@ -263,9 +288,14 @@ function collectStar(player, star) {
         bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
 
     }
-    function  collectHearts(player,hearts){
-                heart.disableBody(true, true);
-                lives += 1
-                livesText.setText('Lives: ' + lives);
-            }
-        }
+    function collectHearts(player, hearts) {
+        heart.disableBody(true, true);
+        lives += 1
+        livesText.setText('Lives: ' + lives);
+    }
+
+    function refreshBody(){
+        console.log('game over')
+        this.scene.restart();
+    }
+}
